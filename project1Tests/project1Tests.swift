7d17faaef30082398fd1a4057c413c56a77a2458
xiaohaofrank <3297103265@qq.com>
2016-03-04 15:27:57 +0800

    xcode代码
    这是上传的xcode代码

---
+++ project1Tests/project1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  project1Tests.swift
+//  project1Tests
+//
+//  Created by 张洪岩 on 16/1/29.
+//  Copyright © 2016年 张洪岩. All rights reserved.
+//
+
+import XCTest
+@testable import project1
+
+class project1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
